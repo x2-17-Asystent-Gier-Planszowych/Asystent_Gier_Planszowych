2ffc7b4c3b89cc45efb26399debf5cefe013d879
Krzysztof Karalus <krzysztof.karalus95@gmail.com>
2017-10-17 16:47:24 +0200

    DataBase

--- Serwer/asystent/src/main/java/com/asystent/Controller/LoginController.java
+++ Serwer/asystent/src/main/java/com/asystent/Controller/LoginController.java
@@ -1,9 +1,12 @@
 package com.asystent.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.ResultSet;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -12,12 +15,17 @@ import java.util.concurrent.atomic.AtomicLong;
 @RestController
 public class LoginController {
 
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
+///https://spring.io/guides/gs/relational-data-access/
         private static final String template = "Logowanie, %s!";
         private final AtomicLong counter = new AtomicLong();
 
         @RequestMapping("/greeting")
         public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
+
+            jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES ('name','last')");
             if(name.equals("Login")) {
                 return new
                         String(template + "succes");

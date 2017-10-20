7c8072272bfb099d34fc85e66185cb051c0b2b66
Filip Kaszuba <fkaszuba93@gmail.com>
2017-10-20 15:04:08 +0200

    main activity tab widget

--- App/AsystentGierPlanszowych/app/src/main/java/pwcdma/asystentgierplanszowych/MainActivity.java
+++ App/AsystentGierPlanszowych/app/src/main/java/pwcdma/asystentgierplanszowych/MainActivity.java
@@ -1,13 +1,31 @@
 package pwcdma.asystentgierplanszowych;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tabHost = (TabHost) findViewById(R.id.tab_host);
+        tabHost.setup();
+        addTab("Grupy", R.id.tab1);
+        addTab("Gry", R.id.tab2);
+        addTab("Mój profil", R.id.tab3);
+    }
+
+    private void addTab(String indicator, int content){
+        TabSpec tab = tabHost.newTabSpec(indicator);
+        tab.setIndicator(indicator);
+        tab.setContent(content);
+        tabHost.addTab(tab);
     }
 }

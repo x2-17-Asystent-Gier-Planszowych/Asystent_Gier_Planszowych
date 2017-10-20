08be2bde8143ba67e33d20bdc8d2f7b99df298f5
Filip Kaszuba <fkaszuba93@gmail.com>
2017-10-20 17:27:01 +0200

    games fragment

--- App/AsystentGierPlanszowych/app/src/main/java/pwcdma/asystentgierplanszowych/MainActivity.java
+++ App/AsystentGierPlanszowych/app/src/main/java/pwcdma/asystentgierplanszowych/MainActivity.java
@@ -1,6 +1,5 @@
 package pwcdma.asystentgierplanszowych;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TabHost;
@@ -8,6 +7,7 @@ import android.widget.TabHost.TabSpec;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAB = "pwcdma.asystentgierplanszowych.tab";
     private TabHost tabHost;
 
     @Override
@@ -20,6 +20,18 @@ public class MainActivity extends AppCompatActivity {
         addTab("Grupy", R.id.tab1);
         addTab("Gry", R.id.tab2);
         addTab("Mój profil", R.id.tab3);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.tab2, new GamesFragment())
+                .commit();
+        if (savedInstanceState != null){
+            tabHost.setCurrentTab(savedInstanceState.getInt(TAB));
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle state) {
+        super.onSaveInstanceState(state);
+        state.putInt(TAB, tabHost.getCurrentTab());
     }
 
     private void addTab(String indicator, int content){

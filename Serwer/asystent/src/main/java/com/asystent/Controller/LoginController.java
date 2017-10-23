28a40c6f967110f88e292aa5164c6a06b51586e1
Krzysztof Karalus <krzysztof.karalus@comarch.pl>
2017-10-23 17:15:03 +0200

    Rejestracja i logowanie bez hashowania

--- Serwer/asystent/src/main/java/com/asystent/Controller/LoginController.java
+++ Serwer/asystent/src/main/java/com/asystent/Controller/LoginController.java
@@ -1,7 +1,10 @@
 package com.asystent.Controller;
 
 
+import com.asystent.Model.Categories;
 import com.asystent.Model.TestModel;
+import com.asystent.Model.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -22,62 +25,90 @@ public class LoginController {
 
     @Autowired
     JdbcTemplate jdbcTemplate;
-    //nie działa to dao trzeba poprawić
-   // DaoImpl dao = new DaoImpl();
-// Poradnik jak korzystać z jdbcTemplate
-///https://spring.io/guides/gs/relational-data-access
-    //aby polaczyc się z baza trzeba zmienic nazwe w application.properties bo narazie dałem testowa
-        private static final String template = "Logowanie, %s!";
-        private final AtomicLong counter = new AtomicLong();
 
-        @RequestMapping("/greeting")
-        public String greeting(@RequestParam(value="name", defaultValue="World") String name) {
-
-           // jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES ('name','last')");
-
-            if(name.equals("Login")) {
-                return new
-                        String(template + "succes");
-            }else{
-                return new
-                        String(template + "false");
-            }
+        
+        
+        @RequestMapping("/rejestacja")
+        public String rejestacja(@RequestParam(value="name", defaultValue="World") String name,
+        		@RequestParam(value="email", defaultValue="World") String email,
+        		@RequestParam(value="password", defaultValue="World") String password) {
+        	
+        	String sqlForUserName = 	"SELECT EXISTS(SELECT 1 FROM \"Users\" WHERE  \"Username\" = ?)";
+        	Boolean isUsedUserName = jdbcTemplate.queryForObject(sqlForUserName, new Object[] {name}, Boolean.class);
+        	
+        	String sqlForEmail = 	"SELECT EXISTS(SELECT 1 FROM \"Users\" WHERE  \"Email\" = ?)";
+        	Boolean isUsedEmail = jdbcTemplate.queryForObject(sqlForEmail, new Object[] {email}, Boolean.class);
+        	
+        	
+        	
+        	 if(isUsedUserName == false) {
+        		 int a =jdbcTemplate.update("INSERT INTO \"Users\" (\"Email\", \"Username\",\"Password\") VALUES (?,?,?)",new Object[]{email,name,password});
+        		    if(a!=0) {
+        		    	return new String("Succes");
+		            }else{
+		                 return new  String("Failed");
+		            }
+        		
+             }else if(isUsedEmail == true) {
+            	 return new
+                         String("Email");
+             }else{
+            	 return new
+                         String("Username");
+             }
+        	 
+        	 
+        
         }
-
+  //  http://localhost:8080/login?login=Login&haslo=haslo
     @RequestMapping("/login")
     public String login(@RequestParam(value="login", defaultValue="World") String login,
                         @RequestParam(value="haslo", defaultValue="World") String haslo) {
-        TestModel testa = new TestModel();
-        //INSERT
-        //jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES ('name11','last')");
-        //SELECT DLA POJEDYNCZEJ DANEJ
-        //String test = jdbcTemplate.queryForObject("SELECT first_name FROM customers where id = 11",String.class);
-        //Select Listy obiektów
-       /* List<TestModel> a = jdbcTemplate.query("select id,first_name,last_name from customers",new RowMapper<TestModel>(){
-            @Override
-            public TestModel mapRow(ResultSet rs, int rownumber) throws SQLException {
-                TestModel e=new TestModel();
-                e.setId(rs.getInt(1));
-                e.setFirst_name(rs.getString(2));
-                e.setLast_name(rs.getString(3));
-                return e;
-            }
-        });*/
-       //DELETE tak samo update
-        // jdbcTemplate.update("DELETE FROM customers where id = 11");
-
-        //Próba odzielenia dao od controllera
-      //  TestModel test = new TestModel(12,"String","String");
-       // dao.insertUser(test);
-
-        if(login.equals("Login")) {
-            return new
-                    String(template + "succes");
+    	
+    	String sqlForUserName = 	"SELECT EXISTS(SELECT 1 FROM \"Users\" WHERE  \"Username\" = ?)";
+    	Boolean isUsedUserName = jdbcTemplate.queryForObject(sqlForUserName, new Object[] {login}, Boolean.class);
+    	String password = null;
+    	
+    	if(isUsedUserName == true){
+    	password = jdbcTemplate.queryForObject("SELECT \"Password\" FROM \"Users\" where \"Username\" = (?)",new Object[]{login}, String.class);
+    	}else{
+    		password = "Mismatch login";
+    	}
+    	
+       if(haslo.equals(password)) {
+            return "Succes";
         }else{
-            return new
-                    String(template + "false");
+            return "Failed";
         }
+    	
     }
 
 
 }
+//nie działa to dao trzeba poprawić
+// DaoImpl dao = new DaoImpl();
+//Poradnik jak korzystać z jdbcTemplate
+///https://spring.io/guides/gs/relational-data-access
+//aby polaczyc się z baza trzeba zmienic nazwe w application.properties bo narazie dałem testowa
+//INSERT
+//jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES ('name11','last')");
+//SELECT DLA POJEDYNCZEJ DANEJ
+//String test = jdbcTemplate.queryForObject("SELECT first_name FROM customers where id = 11",String.class);
+//Select Listy obiektów
+/* List<TestModel> a = jdbcTemplate.query("select id,first_name,last_name from customers",new RowMapper<TestModel>(){
+    @Override
+    public TestModel mapRow(ResultSet rs, int rownumber) throws SQLException {
+        TestModel e=new TestModel();
+        e.setId(rs.getInt(1));
+        e.setFirst_name(rs.getString(2));
+        e.setLast_name(rs.getString(3));
+        return e;
+    }
+});*/
+//DELETE tak samo update
+// jdbcTemplate.update("DELETE FROM customers where id = 11");
+
+//Próba odzielenia dao od controllera
+//  TestModel test = new TestModel(12,"String","String");
+// dao.insertUser(test);
+

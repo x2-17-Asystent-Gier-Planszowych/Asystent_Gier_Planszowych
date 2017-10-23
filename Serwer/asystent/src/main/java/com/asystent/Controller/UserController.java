322638cdbf1f712d26e44ea46c1182f629df10c7
Krzysztof Karalus <krzysztof.karalus95@gmail.com>
2017-10-23 21:38:57 +0200

    Dodanie endpointów do zmiany danych użytkownika

--- Serwer/asystent/src/main/java/com/asystent/Controller/UserController.java
+++ Serwer/asystent/src/main/java/com/asystent/Controller/UserController.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.xml.bind.DatatypeConverter;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -21,7 +25,7 @@ import java.util.concurrent.atomic.AtomicLong;
  * Created by Kamil on 2017-10-17.
  */
 @RestController
-public class LoginController {
+public class UserController {
 
     @Autowired
     JdbcTemplate jdbcTemplate;
@@ -42,8 +46,8 @@ public class LoginController {
         	
         	
         	 if(isUsedUserName == false) {
-        		 int a =jdbcTemplate.update("INSERT INTO \"Users\" (\"Email\", \"Username\",\"Password\") VALUES (?,?,?)",new Object[]{email,name,password});
-        		    if(a!=0) {
+        		 int updateSucces =jdbcTemplate.update("INSERT INTO \"Users\" (\"Email\", \"Username\",\"Password\") VALUES (?,?,?)",new Object[]{email,name,password});
+        		    if(updateSucces!=0) {
         		    	return new String("Succes");
 		            }else{
 		                 return new  String("Failed");
@@ -83,7 +87,50 @@ public class LoginController {
     	
     }
 
+	@RequestMapping("/changePassword")
+	public String changePassword(@RequestParam(value="login", defaultValue="World") String login,
+						@RequestParam(value="haslo", defaultValue="World") String haslo) {
+		int updateSucces = jdbcTemplate.update("UPDATE \"Users\" SET \"Password\" = ? where \"Username\" = ?",new Object[]{haslo,login});
+		if(updateSucces!=0) {
+			return new String("Succes");
+		}else{
+			return new  String("Failed");
+		}
+	}
 
+	@RequestMapping("/changeAbout")
+	public String changeAbout(@RequestParam(value="about", defaultValue="World") String aboutDescription,
+						   @RequestParam(value="login", defaultValue="World") String login) {
+		int updateSucces = jdbcTemplate.update("UPDATE \"Users\" SET \"About\" = ? where \"Username\" = ?",new Object[]{aboutDescription,login});
+		if(updateSucces!=0) {
+			return new String("Succes");
+		}else{
+			return new  String("Failed");
+		}
+	}
+
+	@RequestMapping("/deactivate")
+	public String deactivate(@RequestParam(value="deactivate", defaultValue="World") Boolean deactivate,
+							 @RequestParam(value="login", defaultValue="World") String login
+							 ) {
+		int updateSucces = jdbcTemplate.update("UPDATE \"Users\" SET \"Active\" = ? where \"Username\" = ?",new Object[]{deactivate,login});
+		if(updateSucces!=0) {
+			return new String("Succes");
+		}else{
+			return new  String("Failed");
+		}
+	}
+
+	@RequestMapping("/changeEmail")
+	public String changeEmail(@RequestParam(value="deactivate", defaultValue="World") String email,
+							  @RequestParam(value="login", defaultValue="World") String login) {
+		int updateSucces = jdbcTemplate.update("UPDATE \"Users\" SET \"Email\" = ? where \"Username\" = ?",new Object[]{email,login});
+		if(updateSucces!=0) {
+			return new String("Succes");
+		}else{
+			return new  String("Failed");
+		}
+	}
 }
 //nie działa to dao trzeba poprawić
 // DaoImpl dao = new DaoImpl();

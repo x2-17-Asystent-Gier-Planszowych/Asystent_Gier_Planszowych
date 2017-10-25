383cf804df7af87afbe1f0c8bf7ff060c79cc1a1
fkaszuba93 <fkaszuba93@gmail.com>
2017-10-25 10:27:08 +0200

    remove dummy credentials

--- App/AsystentGierPlanszowych/app/src/main/java/pwcdma/asystentgierplanszowych/activity/LogInActivity.java
+++ App/AsystentGierPlanszowych/app/src/main/java/pwcdma/asystentgierplanszowych/activity/LogInActivity.java
@@ -53,13 +53,6 @@ public class LogInActivity extends AppCompatActivity implements LoaderCallbacks<
     private static final int REQUEST_READ_CONTACTS = 0;
 
     /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
      * Keep track of the login task to ensure we can cancel it if requested.
      */
     private UserLoginTask mAuthTask = null;
@@ -348,14 +341,6 @@ public class LogInActivity extends AppCompatActivity implements LoaderCallbacks<
                 Thread.sleep(2000);
             } catch (InterruptedException e) {
                 return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
             }
 
             return true;
